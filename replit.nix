9a77c8c8f84e7fd290458ebdbb91bcc453783e19
Emmy R <112522629+emmy-rich@users.noreply.github.com>
2022-11-04 18:54:47 +0000

    Worked on database.

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+	deps = [
+		pkgs.python39Full
+  pkgs.python39Full
+  pkgs.nodejs-18_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.yarn
+        pkgs.replitPackages.jest
+	];
+}
